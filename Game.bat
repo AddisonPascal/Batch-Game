6814e5730245d0b56d590f16e2bbb78dec7c4cfa
AddisonPascal <addisondj@hotmail.com>
2019-03-01 12:09:14 +1100

    Made so it saves data to a folder so you can resume, or play multiplayer.

--- Game.bat
+++ Game.bat
@@ -7,20 +7,290 @@ echo start /max Game.bat^&exit
 start tmp_max.bat&exit
 :start
 del tmp_max.bat
+set GameData=GameData
+md %GameData%
 set seenChar=.
 set name=tmp_undefined
 setlocal enabledelayedexpansion
-call data.bat
 set version=0.0.0
 title Batch Game
-if "!name!"=="tmp_undefined" set/p name="Username: "
-(
-echo set name=!name!
-)>data.bat
+cls
+set/p name="Username: "
 set playerX=0
 set playerY=0
 set points=0
+call %GameData%\user%name%.bat
+(
+@echo off
+echo set playerX=%playerX%
+echo set playerY=%playerY%
+echo set points=%points%
+)>"%GameData%\user%name%.bat"
 :update
+::
+set /a writeX=%playerX%-5
+set /a writeY=%playerY%-5
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+5
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+::
+set /a writeX=%playerX%-5
+set /a writeY=%playerY%-4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+5
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+::
+set /a writeX=%playerX%-5
+set /a writeY=%playerY%-3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+5
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+::
+set /a writeX=%playerX%-5
+set /a writeY=%playerY%-2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+5
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+::
+set /a writeX=%playerX%-5
+set /a writeY=%playerY%-1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+5
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+::
+set /a writeX=%playerX%-5
+set /a writeY=%playerY%
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+5
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+::
+set /a writeX=%playerX%-5
+set /a writeY=%playerY%+1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+5
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+::
+set /a writeX=%playerX%-5
+set /a writeY=%playerY%+2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+5
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+::
+set /a writeX=%playerX%-5
+set /a writeY=%playerY%+3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+5
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+::
+set /a writeX=%playerX%-5
+set /a writeY=%playerY%+4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+5
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+::
+set /a writeX=%playerX%-5
+set /a writeY=%playerY%+5
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%-1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+1
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+2
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+3
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+4
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+set /a writeX=%playerX%+5
+if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
 :: Makes empty spaces  
 ::
 set /a writeX=%playerX%-5
@@ -291,22 +561,30 @@ set x%playerX%,y%playerY%=!name:~0,1!
 set /a writeX=%playerX%-1
 set /a writeY=%playerY%+1
 set x%writeX%,y%writeY%=+
+call :savePlus
 set /a writeX=%playerX%
 set x%writeX%,y%writeY%=+
+call :savePlus
 set /a writeX=%playerX%+1
 set x%writeX%,y%writeY%=+
+call :savePlus
 set /a writeX=%playerX%-1
 set /a writeY=%playerY%
 set x%writeX%,y%writeY%=+
+call :savePlus
 set /a writeX=%playerX%+1
 set x%writeX%,y%writeY%=+
+call :savePlus
 set /a writeX=%playerX%-1
 set /a writeY=%playerY%-1
 set x%writeX%,y%writeY%=+
+call :savePlus
 set /a writeX=%playerX%+1
 set x%writeX%,y%writeY%=+
+call :savePlus
 set /a writeX=%playerX%
 set x%writeX%,y%writeY%=+
+call :savePlus
 :: Sets coordinates to plot on the screen
 ::
 set /a writeX=%playerX%-5
@@ -593,50 +871,107 @@ echo Loading...
 set /a writeX=%playerX%-1
 set /a writeY=%playerY%+1
 set x%writeX%,y%writeY%= 
+call :saveSpace
 set /a writeX=%playerX%
 set x%writeX%,y%writeY%= 
+call :saveSpace
 set /a writeX=%playerX%+1
 set x%writeX%,y%writeY%= 
+call :saveSpace
 set /a writeX=%playerX%-1
 set /a writeY=%playerY%
 set x%writeX%,y%writeY%= 
+call :saveSpace
 set /a writeX=%playerX%+1
 set x%writeX%,y%writeY%= 
+call :saveSpace
 set /a writeX=%playerX%-1
 set /a writeY=%playerY%-1
 set x%writeX%,y%writeY%= 
+call :saveSpace
 set /a writeX=%playerX%+1
 set x%writeX%,y%writeY%= 
+call :saveSpace
 set /a writeX=%playerX%
 set x%writeX%,y%writeY%= 
-if %errorlevel%==1 set /a playerY=%playerY%+1
-if %errorlevel%==3 set /a playerY=%playerY%-1
-if %errorlevel%==2 set /a playerX=%playerX%-1
-if %errorlevel%==4 set /a playerX=%playerX%+1
+call :saveSpace
 if %random:~-1,1% GEQ 5 (
 set /a goldX=%playerX%-5-%random:~-1,1%
 if %random:~-1,1% GEQ 5 set /a goldX=%playerX%+5+%random:~-1,1%
 set /a goldY=%playerY%-5-%random:~-1,1%
 if %random:~-1,1% GEQ 5 set /a goldY=%playerY%+5+%random:~-1,1%
 if "!x%goldX%,y%goldY%!"=="" set x%goldX%,y%goldY%=$
+if "!x%goldX%,y%goldY%!"=="$" call :saveGold
 )
+if %errorlevel%==1 set /a playerY=%playerY%+1
+if %errorlevel%==3 set /a playerY=%playerY%-1
+if %errorlevel%==2 set /a playerX=%playerX%-1
+if %errorlevel%==4 set /a playerX=%playerX%+1
 set /a writeX=%playerX%+1
 set /a writeY=%playerY%+1
 if "!x%writeX%,y%writeY%!"=="$" set /a points=%points%+1
+if "!x%writeX%,y%writeY%!"=="$" (
+set "x%writeX%,y%writeY%= "
+call :saveSpace
+)
 set /a writeX=%playerX%
 if "!x%writeX%,y%writeY%!"=="$" set /a points=%points%+1
+if "!x%writeX%,y%writeY%!"=="$" (
+set "x%writeX%,y%writeY%= "
+call :saveSpace
+)
 set /a writeX=%playerX%-1
 if "!x%writeX%,y%writeY%!"=="$" set /a points=%points%+1
+if "!x%writeX%,y%writeY%!"=="$" (
+set "x%writeX%,y%writeY%= "
+call :saveSpace
+)
 set /a writeX=%playerX%+1
 set /a writeY=%playerY%-1
 if "!x%writeX%,y%writeY%!"=="$" set /a points=%points%+1
+if "!x%writeX%,y%writeY%!"=="$" (
+set "x%writeX%,y%writeY%= "
+call :saveSpace
+)
 set /a writeX=%playerX%
 if "!x%writeX%,y%writeY%!"=="$" set /a points=%points%+1
+if "!x%writeX%,y%writeY%!"=="$" (
+set "x%writeX%,y%writeY%= "
+call :saveSpace
+)
 set /a writeX=%playerX%-1
 if "!x%writeX%,y%writeY%!"=="$" set /a points=%points%+1
+if "!x%writeX%,y%writeY%!"=="$" (
+set "x%writeX%,y%writeY%= "
+call :saveSpace
+)
 set /a writeX=%playerX%+1
 set /a writeY=%playerY%
 if "!x%writeX%,y%writeY%!"=="$" set /a points=%points%+1
+if "!x%writeX%,y%writeY%!"=="$" (
+set "x%writeX%,y%writeY%= "
+call :saveSpace
+)
 set /a writeX=%playerX%-1
 if "!x%writeX%,y%writeY%!"=="$" set /a points=%points%+1
+if "!x%writeX%,y%writeY%!"=="$" (
+set "x%writeX%,y%writeY%= "
+call :saveSpace
+)
+(
+@echo off
+echo set playerX=%playerX%
+echo set playerY=%playerY%
+echo set points=%points%
+)>"%GameData%\user%name%.bat"
 goto update
+
+:saveGold
+echo set x%goldX%,y%goldY%=$>%GameData%\x%goldX%^,y%goldY%.bat
+exit/b
+:saveSpace
+echo set "x%writeX%,y%writeY%= ">%GameData%\x%writeX%^,y%writeY%.bat
+exit/b
+:savePlus
+echo set x%writeX%,y%writeY%=+>%GameData%\x%writeX%^,y%writeY%.bat
+exit/b
