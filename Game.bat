80e3b7d2d7644aec3fc36f1dbf2c364349833cf7
AddisonPascal <addisondj@hotmail.com>
2019-03-02 15:20:16 +1100

    Improved loading graphics to appear smoother

--- Game.bat
+++ Game.bat
@@ -148,6 +148,7 @@ set /a writeX=%playerX%+4
 if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
 set /a writeX=%playerX%+5
 if exist "%GameData%\x%writeX%,y%writeY%.bat" call "%GameData%\x%writeX%,y%writeY%.bat"
+call :%move%
 ::
 set /a writeX=%playerX%-5
 set /a writeY=%playerY%
@@ -938,10 +939,23 @@ if %random:~-1,1% GEQ 5 set /a goldY=%playerY%+5+%random:~-1,1%
 if "!x%goldX%,y%goldY%!"=="" set x%goldX%,y%goldY%=$
 if "!x%goldX%,y%goldY%!"=="$" call :saveGold
 )
-if %errorlevel%==1 set /a playerY=%playerY%+1
-if %errorlevel%==3 set /a playerY=%playerY%-1
-if %errorlevel%==2 set /a playerX=%playerX%-1
-if %errorlevel%==4 set /a playerX=%playerX%+1
+set move=no
+if %errorlevel%==1 (
+set /a playerY=%playerY%+1
+set move=forward
+)
+if %errorlevel%==3 (
+set /a playerY=%playerY%-1
+set move=backward
+)
+if %errorlevel%==2 (
+set /a playerX=%playerX%-1
+set move=left
+)
+if %errorlevel%==4 (
+set /a playerX=%playerX%+1
+set move=right
+)
 if %errorlevel%==5 (
 set /p cont="Teleport to X: "
 set /p cont2="Teleport to Y: "
@@ -1021,4 +1035,63 @@ echo set x%writeX%,y%writeY%=+>%GameData%\x%writeX%^,y%writeY%.bat
 exit/b
 :savePlayer
 echo set x%playerX%,y%playerY%=!name:~0,1!>%GameData%\x%playerX%^,y%playerY%.bat
+exit /b
+
+:no
+exit/b
+
+:forward
+exit/b
+
+:backward
+exit/b
+
+:right
+cls
+echo. 
+echo _______________________
+echo ^|!px1,y10! !px2,y10! !px3,y10! !px4,y10! !px5,y10! !px6,y10! !px7,y10! !px8,y10! !px9,y10! !px10,y10!  ^|
+echo ^|!px1,y9! !px2,y9! !px3,y9! !px4,y9! !px5,y9! !px6,y9! !px7,y9! !px8,y9! !px9,y9! !px10,y9!  ^|
+echo ^|!px1,y8! !px2,y8! !px3,y8! !px4,y8! !px5,y8! !px6,y8! !px7,y8! !px8,y8! !px9,y8! !px10,y8!  ^|
+echo ^|!px1,y7! !px2,y7! !px3,y7! !px4,y7! !px5,y7! !px6,y7! !px7,y7! !px8,y7! !px9,y7! !px10,y7!  ^|
+echo ^|!px1,y6! !px2,y6! !px3,y6!  !px4,y6! !px5,y6! !px6,y6!!px7,y6! !px8,y6! !px9,y6! !px10,y6!  ^|
+echo ^|!px1,y5! !px2,y5! !px3,y5!  !px4,y5! !px5,y5! !px6,y5!!px7,y5! !px8,y5! !px9,y5! !px10,y5!  ^|
+echo ^|!px1,y4! !px2,y4! !px3,y4!  !px4,y4! !px5,y4! !px6,y4!!px7,y4! !px8,y4! !px9,y4! !px10,y4!  ^|
+echo ^|!px1,y3! !px2,y3! !px3,y3! !px4,y3! !px5,y3! !px6,y3! !px7,y3! !px8,y3! !px9,y3! !px10,y3!  ^|
+echo ^|!px1,y2! !px2,y2! !px3,y2! !px4,y2! !px5,y2! !px6,y2! !px7,y2! !px8,y2! !px9,y2! !px10,y2!  ^|
+echo ^|!px1,y1! !px2,y1! !px3,y1! !px4,y1! !px5,y1! !px6,y1! !px7,y1! !px8,y1! !px9,y1! !px10,y1!  ^|
+echo Points: %points%
+echo Player coordinates: (x=%playerX%, y=%playerY%)
+echo. 
+echo. 
+echo Press P to track another player.
+echo Tracking: %tracking%
+echo %tracking% X=%trackedPX%, Y=%trackedPY%
+echo %tracking% has %trackedPt% points.
+echo You are %Dist% blocks away from %tracking%.
+exit /b
+
+:left
+cls
+echo. 
+echo _______________________
+echo ^|  !px1,y10! !px2,y10! !px3,y10! !px4,y10! !px5,y10! !px6,y10! !px7,y10! !px8,y10! !px9,y10! !px10,y10!^|
+echo ^|  !px1,y9! !px2,y9! !px3,y9! !px4,y9! !px5,y9! !px6,y9! !px7,y9! !px8,y9! !px9,y9! !px10,y9!^|
+echo ^|  !px1,y8! !px2,y8! !px3,y8! !px4,y8! !px5,y8! !px6,y8! !px7,y8! !px8,y8! !px9,y8! !px10,y8!^|
+echo ^|  !px1,y7! !px2,y7! !px3,y7! !px4,y7! !px5,y7! !px6,y7! !px7,y7! !px8,y7! !px9,y7! !px10,y7!^|
+echo ^|  !px1,y6! !px2,y6! !px3,y6!!px4,y6! !px5,y6! !px6,y6!  !px7,y6! !px8,y6! !px9,y6! !px10,y6!^|
+echo ^|  !px1,y5! !px2,y5! !px3,y5!!px4,y5! !px5,y5! !px6,y5!  !px7,y5! !px8,y5! !px9,y5! !px10,y5!^|
+echo ^|  !px1,y4! !px2,y4! !px3,y4!!px4,y4! !px5,y4! !px6,y4!  !px7,y4! !px8,y4! !px9,y4! !px10,y4!^|
+echo ^|  !px1,y3! !px2,y3! !px3,y3! !px4,y3! !px5,y3! !px6,y3! !px7,y3! !px8,y3! !px9,y3! !px10,y3!^|
+echo ^|  !px1,y2! !px2,y2! !px3,y2! !px4,y2! !px5,y2! !px6,y2! !px7,y2! !px8,y2! !px9,y2! !px10,y2!^|
+echo ^|  !px1,y1! !px2,y1! !px3,y1! !px4,y1! !px5,y1! !px6,y1! !px7,y1! !px8,y1! !px9,y1! !px10,y1!^|
+echo Points: %points%
+echo Player coordinates: (x=%playerX%, y=%playerY%)
+echo. 
+echo. 
+echo Press P to track another player.
+echo Tracking: %tracking%
+echo %tracking% X=%trackedPX%, Y=%trackedPY%
+echo %tracking% has %trackedPt% points.
+echo You are %Dist% blocks away from %tracking%.
 exit /b
